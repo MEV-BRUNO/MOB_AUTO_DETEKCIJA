37a4c3180e85f85cdfa0cb2c89b17cc40ddecc90
Kristijan Pravica <kristijan.pravica@gmail.com>
2018-06-20 21:25:52 +0200

    Dodane skoro sve slike u svim rezolucijama i dodani xml selektori za toggle slike

--- app/src/main/java/kristijan/autodetekcija/MainActivity.java
+++ app/src/main/java/kristijan/autodetekcija/MainActivity.java
@@ -1,7 +1,9 @@
 package kristijan.autodetekcija;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void detekcijaKvara(View view){
+        Intent intent = new Intent(this, detekcijaKvaraActivity.class);
+        startActivity(intent);
+    }
+
+    public void provjeriSam(View view){
+        Intent intent = new Intent(this, ProvjeriSamActivity.class);
+        startActivity(intent);
+    }
+
+    public void pronadiServis(View view){
+        Intent intent = new Intent(this, PronadiServisActivity.class);
+        startActivity(intent);
+    }
+
+    public void akcije(View view){
+        Intent intent = new Intent(this, AkcijeActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/kristijan/autodetekcija/PronadiServisActivity.java
@@ -0,0 +1,13 @@
+package kristijan.autodetekcija;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PronadiServisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pronadi_servis);
+    }
+}
